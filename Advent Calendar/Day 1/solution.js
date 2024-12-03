4f92ee7ff40e344e4ce753d4d66c32c355ddf076
whitmank <whitman.karter@gmail.com>
2024-12-03 17:50:50 -0500

    Solved

--- Advent Calendar/Day 1/solution.js
+++ Advent Calendar/Day 1/solution.js
@@ -1,39 +1,81 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
-// Read file
-var input = fs.readFileSync('input.txt', 'utf8');
-// Parse each line of the file into an
-// array of objects => locations[] = {num1, num2}
-var locations = input.split('\n')
-    .filter(function (line) { return line.trim() !== ''; }) // Filter out empty lines
-    .map(function (line) {
-    var _a = line
-        .trim()
-        .split(/\s+/)
-        .map(Number), num1 = _a[0], num2 = _a[1];
-    // console.log('Parsed numbers:', { num1, num2 });
-    return { num1: num1, num2: num2 };
-});
-// sort the lists from least to greatest
-var sortedNum1 = locations.map(function (loc) { return loc.num1; }).sort(function (a, b) { return a - b; });
-var sortedNum2 = locations.map(function (loc) { return loc.num2; }).sort(function (a, b) { return a - b; });
-console.log("sortedNum1:", sortedNum1, sortedNum2);
-// Distance
-var distanceSum = 0;
-for (var i = 0; i < sortedNum1.length; i++) {
-    distanceSum += Math.abs(sortedNum1[i] - sortedNum2[i]);
+/** TODO
+ * Processes input from a text file of numbers to an array.
+ * @param puzzleInput path to input.txt
+ * @returns locations[{ num1, num2 }]
+ */
+function parseInput(puzzleInput) {
+    // Read file
+    var input = fs.readFileSync(puzzleInput, 'utf-8');
+    // Parse each line of the file into an array of objects => locations[] = {num1, num2}
+    var locations = input
+        .split('\n')
+        .filter(function (line) { return line.trim() !== ''; }) // Filter out empty lines
+        .map(function (line) {
+        var _a = line
+            .trim()
+            .split(/\s+/)
+            .map(Number), num1 = _a[0], num2 = _a[1];
+        return { num1: num1, num2: num2 };
+    });
+    return locations;
 }
-// Similarity Score
-var similarityScore = 0;
-for (var i = 0; i < sortedNum1.length; i++) {
-    var freqNum1 = 0;
-    for (var j = 0; j < sortedNum2.length; j++) {
-        if (sortedNum1[i] == sortedNum2[j]) {
-            freqNum1++;
-        }
+/** TODO
+ * Sorts the location array by splitting num1 and num2 components into separate arrays.
+ * @param parsedLocations[]
+ * @returns sortedLocations = { sortedNum1: number[], sortedNum2: number[] }
+ */
+function sortLists(locations) {
+    // Split array into two lists, and sort each one in ascending order
+    var sortedNum1 = locations.map(function (loc) { return loc.num1; }).sort(function (a, b) { return a - b; });
+    var sortedNum2 = locations.map(function (loc) { return loc.num2; }).sort(function (a, b) { return a - b; });
+    var sortedLocations = { sortedNum1: sortedNum1, sortedNum2: sortedNum2 };
+    return sortedLocations;
+}
+/** TODO
+ * Computes sum of differences between each corresponding pair {sortedNum1, sortedNum2}.
+ * @param { sortedNum1[], sortedNum2[] }
+ * @returns number distanceSum
+ */
+function computeSumOfDistances(sortedLocations) {
+    var distanceSum = 0;
+    var sortedNum1 = sortedLocations.sortedNum1;
+    var sortedNum2 = sortedLocations.sortedNum2;
+    for (var i = 0; i < sortedNum1.length; i++) {
+        distanceSum += Math.abs(sortedNum1[i] - sortedNum2[i]);
     }
-    similarityScore += sortedNum1[i] * freqNum1;
+    return distanceSum;
 }
-console.log("Distance", distanceSum);
+/** TODO
+ * Computes similarity score of each corresponding pair {sortedNum1, sortedNum2}.
+ * @param { sortedNum1[], sortedNum2[] }
+ * @returns number similarityScore
+ */
+function computeSimilarityScore(sortedLocations) {
+    // Compute Similarity Score
+    var similarityScore = 0;
+    var sortedNum1 = sortedLocations.sortedNum1;
+    var sortedNum2 = sortedLocations.sortedNum2;
+    for (var i = 0; i < sortedNum1.length; i++) {
+        var freqNum1 = 0;
+        for (var j = 0; j < sortedNum2.length; j++) {
+            if (sortedNum1[i] == sortedNum2[j]) {
+                freqNum1++;
+            }
+        }
+        similarityScore += sortedNum1[i] * freqNum1;
+    }
+    return similarityScore;
+}
+/****************************************************************************************************/
+/* Run program */
+/****************************************************************************************************/
+var inputFilePath = './input.txt';
+var locations = parseInput(inputFilePath);
+var sortedLocations = sortLists(locations);
+var distanceSum = computeSumOfDistances(sortedLocations);
+var similarityScore = computeSimilarityScore(sortedLocations);
+console.log("Distance:", distanceSum);
 console.log("Similarity Score:", similarityScore);
--- Advent Calendar/Day 1/solution.ts
+++ Advent Calendar/Day 1/solution.ts
@@ -2,10 +2,10 @@ import * as fs from 'fs';
 
 /** TODO
  * Processes input from a text file of numbers to an array.
- * @param puzzleInput 
+ * @param puzzleInput path to input.txt
  * @returns locations[{ num1, num2 }]
  */
-function parseInput(puzzleInput: File) {
+function parseInput(puzzleInput: string): Array<{ num1: number; num2: number }> {
     // Read file
     const input = fs.readFileSync(puzzleInput, 'utf-8')
     // Parse each line of the file into an array of objects => locations[] = {num1, num2}
@@ -17,33 +17,36 @@ function parseInput(puzzleInput: File) {
                 .trim()
                 .split(/\s+/)
                 .map(Number);
-            // console.log('Parsed numbers:', { num1, num2 });
             return { num1, num2 };
         })
+        return locations;
 }
 
 /** TODO
  * Sorts the location array by splitting num1 and num2 components into separate arrays.
- * @param locations[]
- * @returns sortedNum1[]
- * @returns sortedNum2[]
+ * @param parsedLocations[]
+ * @returns sortedLocations = { sortedNum1: number[], sortedNum2: number[] }
  */
 
-function sortLists() {
+function sortLists(locations: Array<{ num1: number; num2: number }>): { sortedNum1: number[]; sortedNum2: number[]; } {
+
     // Split array into two lists, and sort each one in ascending order
     const sortedNum1 = locations.map(loc => loc.num1).sort((a, b) => a - b);
     const sortedNum2 = locations.map(loc => loc.num2).sort((a, b) => a - b);
+    const sortedLocations = { sortedNum1, sortedNum2 };
+    return sortedLocations
 }
 
 /** TODO 
  * Computes sum of differences between each corresponding pair {sortedNum1, sortedNum2}.
- * @param sortedNum1[]
- * @param sortedNum2[]
- * @returns Int distanceSum
+ * @param { sortedNum1[], sortedNum2[] }
+ * @returns number distanceSum
  */
-function computeSumOfDistances() {
+function computeSumOfDistances(sortedLocations: { sortedNum1: number[], sortedNum2: number[]}): number{
 
     let distanceSum = 0;
+    let sortedNum1 = sortedLocations.sortedNum1;
+    let sortedNum2 = sortedLocations.sortedNum2;
 
     for (let i = 0; i < sortedNum1.length; i++) {
         distanceSum += Math.abs(sortedNum1[i] - sortedNum2[i])
@@ -53,14 +56,15 @@ function computeSumOfDistances() {
 
 /** TODO
  * Computes similarity score of each corresponding pair {sortedNum1, sortedNum2}.
- * @param sortedNum1[]
- * @param sortedNum2[]
- * @returns Int distanceSum
+ * @param { sortedNum1[], sortedNum2[] }
+ * @returns number similarityScore
  */
-function computeSimilarityScore() {
+function computeSimilarityScore(sortedLocations: { sortedNum1: number[], sortedNum2: number[]}): number {
 
     // Compute Similarity Score
     let similarityScore = 0;
+    let sortedNum1 = sortedLocations.sortedNum1;
+    let sortedNum2 = sortedLocations.sortedNum2;
 
     for (let i = 0; i < sortedNum1.length; i++) {
         let freqNum1 = 0;
@@ -77,10 +81,12 @@ function computeSimilarityScore() {
 /****************************************************************************************************/
 /* Run program */
 /****************************************************************************************************/
+const inputFilePath = './input.txt'
+const locations = parseInput(inputFilePath)
+const sortedLocations = sortLists(locations)
 
-parseInput();
-computeSumOfDistances();
-computeSimilarityScore();
+let distanceSum = computeSumOfDistances(sortedLocations)
+let similarityScore = computeSimilarityScore(sortedLocations)
 
 console.log("Distance:", distanceSum);
 console.log("Similarity Score:", similarityScore);
